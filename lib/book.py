8647a63a468f482ca2d727193df09c8d4c08cef6
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-09-22 18:53:26 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env python3
+# lib/book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # page_count property
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # turn_page method
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"  # Default condition when the shoe is created
+
+    # size property
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    # cobble method
+    def cobble(self):
+        self.condition = "New"  # Set condition to "New" when cobble is called
+        print("Your shoe is as good as new!")
